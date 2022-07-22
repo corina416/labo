aeb2a50d8d0a6ad6b32bf2ec569c7204c4d0ea5c
CorinaAlarcon <corina.alarcon@gmail.com>
2022-07-21 21:31:42 -0300

    cluster 8134

--- src/clustering/761_cluster_jerarquico.r
+++ src/clustering/761_cluster_jerarquico.r
@@ -97,7 +97,7 @@ dataset[  , .N,  cluster2 ]  #tamaño de los clusters
 
 #grabo el dataset en el bucket, luego debe bajarse a la PC y analizarse
 fwrite( dataset,
-        file= "cluster_de_bajas.txt",
+        file= "cluster_de_bajas8134a.txt",
         sep= "\t" )
 
 
